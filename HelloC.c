6d972d09a5b5ba0fc0ef2a39fabc9424dcbd75f5
BaronQi <qibenyu@163.com>
2020-06-09 22:11:22 +0800

    first commit

---
+++ HelloC.c
@@ -0,0 +1,56 @@
+
+#include <stdio.h>
+
+#define WIDTH 80
+#define LENGTH WIDTH + 40
+#define pi 3.14
+#define r 1
+
+#define ADD(x, y) x + y;
+
+int main()
+{
+    int a[2][3];
+    int b[][3] = {0,1,2,3};
+    int c[100][100]={0};
+    int d[3][]={{1,2},{1,2,3},{1,2,3,4}};
+}
+
+// int a = 3;
+// int f(int x)
+// {
+//     int a = 13;
+//     return (x + a);
+// }
+
+// main()
+// {
+//     int x = 2;
+//     printf("%d\n", f(x) / a);
+// }
+
+// int main()
+// {
+//     int a = 2, b = -1, c = 2;
+//     if (a < b)
+//         if (b < 0)
+//             c = 0;
+//         else
+//             c += 1;
+//     printf("%d\n", c);
+//     return 0;
+// }
+// int v = LENGTH * 20;
+// int b = 7;
+// float a = 2.5, c = 4.7;
+
+// float result = a + (int)(b / 3 * (int)(a + c) / 2) % 4;
+// printf("result = %f \r\n", result);
+// printf("Hello World ");
+
+// getNumber();
+
+// void getNumber()
+// {
+//     printf("Hello Number");
+// }
